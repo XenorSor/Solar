67681a992bdd2f7d0f17541b9f677d8ee50cdc9e
Biró Tamás <biro.tamas@stud.uni-obuda.hu>
2024-12-12 01:45:41 +0100

    fix big

--- SolarPrediction_Wep_App.py
+++ SolarPrediction_Wep_App.py
@@ -18,20 +18,12 @@ def main():
     Date = st.date_input('Date of measurement')
     Time = st.time_input('Time of measurement')
     Temperature = st.number_input('Temperature (°F)', step=1., min_value=-459., max_value=10000., value=20., format='%.2f')
-    Pressure_f = st.number_input('Barometric pressure (Hg)2', step=0.01, format='%.2f'#, min_value=29, max_value=31, value=30, format='%.2f'
-                                 )
+    Pressure_f = st.number_input('Barometric pressure (Hg)2', step=0.01, min_value=29, max_value=31, value=30, format='%.2f')
     Humidity = st.number_input('Humidity percent', step=1., min_value=0., max_value=120., value=30., format='%.2f')
     WindDirection = st.number_input('Wind direction (Degrees)', step=1., min_value=0., max_value=360., value=180., format='%.2f')
     Speed = st.number_input('Wind speed (miles per hour)', step=1., min_value=0., max_value=300., value=5., format='%.2f')
     TimeSunRise = st.time_input('Sunrise (Hawaii time)')
     TimeSunSet = st.time_input('Sunset (Hawaii time)')
-
-    float_value = st.number_input(
-        "Number",
-        step=0.01,
-    )
-    int_value = round(float_value * 100)
-    st.write(int_value, float_value)
 
     prediction = ''
 
