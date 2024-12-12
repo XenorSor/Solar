35e0db76dac7dd467f1b90cba5e043a2e6961067
Biró Tamás <biro.tamas@stud.uni-obuda.hu>
2024-12-12 01:48:46 +0100

    as

--- SolarPrediction_Wep_App.py
+++ SolarPrediction_Wep_App.py
@@ -6,7 +6,7 @@ loaded_model = pickle.load(open('solar_pred.sav', 'rb'))
 
 def solar_prediction(input_data):
     input_data_as_numpy_array = np.asarray(input_data)
-    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
+    #input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
 
     prediction = loaded_model.predict(input_data_reshaped)
 
