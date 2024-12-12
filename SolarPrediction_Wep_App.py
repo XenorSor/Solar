6b39a755c13b8e740729771761c696ea43a1ad18
Biró Tamás <biro.tamas@stud.uni-obuda.hu>
2024-12-12 01:55:52 +0100

    fix

--- SolarPrediction_Wep_App.py
+++ SolarPrediction_Wep_App.py
@@ -18,7 +18,7 @@ def main():
     Date = st.date_input('Date of measurement')
     Time = st.time_input('Time of measurement')
     Temperature = st.number_input('Temperature (°F)', step=1., min_value=-459., max_value=10000., value=20., format='%.2f')
-    Pressure_f = st.number_input('Barometric pressure (Hg)2', step=0.01, min_value=29., max_value=31., value=30., format='%.2f')
+    Pressure = st.number_input('Barometric pressure (Hg)2', step=0.01, min_value=29., max_value=31., value=30., format='%.2f')
     Humidity = st.number_input('Humidity percent', step=1., min_value=0., max_value=120., value=30., format='%.2f')
     WindDirection = st.number_input('Wind direction (Degrees)', step=1., min_value=0., max_value=360., value=180., format='%.2f')
     Speed = st.number_input('Wind speed (miles per hour)', step=1., min_value=0., max_value=300., value=5., format='%.2f')
@@ -27,8 +27,21 @@ def main():
 
     prediction = ''
 
+    D_Y = Date.year
+    D_M = Date.month
+    D_D = Date.day
+    T_H = Time.hour
+    T_M = Time.minute
+    T_S = Time.second
+    R_H = TimeSunRise.hour
+    R_M = TimeSunRise.minute
+    R_S = TimeSunRise.second
+    S_H = TimeSunSet.hour
+    S_M = TimeSunSet.minute
+    S_S = TimeSunSet.second
+
     if st.button('Solar Prediction Result'):
-        prediction = solar_prediction([Date, Time, Temperature, Pressure_f, Humidity, WindDirection, Speed, TimeSunRise, TimeSunSet])
+        prediction = solar_prediction([D_Y, D_M, D_D, T_H, T_M, T_S, Temperature, Pressure, Humidity, WindDirection, Speed, R_H, R_M, R_S, S_H, S_M, S_S])
 
     st.success(prediction)
 
